610bef1020fbe50c000f0de4742786f082ea1066
Thiago Chagas <tqchagas@gmail.com>
2018-11-03 18:08:24 -0300

    allowed hosts

--- clientes_microsservico/settings.py
+++ clientes_microsservico/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_59k8n!ygm%0u(lcoz-+xrbi47w52lz)bs2_#kk!!a+h)rwa!j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['clientes-microsservico.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
